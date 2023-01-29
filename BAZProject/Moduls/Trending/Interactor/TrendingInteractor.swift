f847064af0b705beb73b2308094ce1f667956a73
GERARDO BAUTISTA CASTANEDA <gerardo.bautistac@elektra.com.mx>
2023-01-28 22:53:36 -0600

    commit

---
+++ BAZProject/Moduls/Trending/Interactor/TrendingInteractor.swift
@@ -0,0 +1,46 @@
+//
+//  TrendingViewInteractor.swift
+//  BAZProject
+//
+//  Created by 1058889 on 26/01/23.
+//
+
+import Foundation
+
+typealias TrendingInteractorProtocol = TrendingViewInteractorInputProtocol & TrendingViewInteractorOutputProtocol
+class TrendingInteractor: TrendingInteractorProtocol {
+    var presenter: TrendingViewInteractorOutputProtocol?
+    var providerNetworking: NetworkingProviderProtocol = NetworkingProviderService.shared
+    
+    func getTrendingMedia(mediaType: MediaType, timeWindow: TimeWindowType) {
+        
+        let endPoint: String = "/trending/\(mediaType.rawValue)/\(timeWindow.rawValue)"
+        let strUrl: String = endPoint.getStrUrlTheMovieDb()
+        
+        providerNetworking.sendRequest(
+            requestType: RequestType(strUrl: strUrl, method: .GET)
+        ) { success, data in
+            DispatchQueue.main.async {
+                var result: [MovieResult]?
+                if let data = data {
+                    do {
+                        let response = try JSONDecoder().decode(MovieResponse.self, from: data)
+                        result = response.results
+                    } catch {
+                        result = nil
+                    }
+                }
+                self.presenter?.getTrendingMedia(success: success, result: result)
+                
+//                self.presenter.sucess()
+//                
+//                self.presenter.fail()
+//                dataManager -> 
+            }
+        }
+    }
+    
+    func getTrendingMedia(success: Bool, result: [MovieResult]?) {
+        presenter?.getTrendingMedia(success: success, result: result)
+    }
+}
