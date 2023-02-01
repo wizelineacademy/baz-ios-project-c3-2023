3d71d964dd5c4c1e179d31a0fbdf59e9257b02ec
GERARDO BAUTISTA CASTANEDA <gerardo.bautistac@elektra.com.mx>
2023-02-01 10:30:40 -0600

    add skeleton animation extention

--- BAZProject/Util/Extensions/UIImage+Extension.swift
+++ BAZProject/Util/Extensions/UIImage+Extension.swift
@@ -10,25 +10,29 @@ import UIKit
 let cache = NSCache<NSString, UIImage>()
 extension UIImageView {
     func loadImage(id: String) {
-        
+        self.addSkeletonAnimation()
         image = UIImage()
         let defaultImage = UIImage(named: "poster")
         let strUrl = id
         if let img = cache.object(forKey: NSString(string: strUrl)) {
+            self.removeSkeletonAnimation()
             image = img
             return
         }
         
         guard let url = URL(string: strUrl) else {
+            self.removeSkeletonAnimation()
             image = defaultImage
             return
         }
         
         URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
             if let _ = error {
+                self?.removeSkeletonAnimation()
                 self?.image = defaultImage
             } else {
                 DispatchQueue.main.async {
+                    self?.removeSkeletonAnimation()
                     guard let data = data,
                           let tempImg = UIImage(data: data) else { return }
                     self?.alpha = 0.2;
