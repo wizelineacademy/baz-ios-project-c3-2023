dd0581e7fa4e7c62efa9b14e4311284f59183db5
Esmeralda Angeles <Esmeralda.angeles@bancoazteca.com>
2023-02-01 22:26:36 -0600

    fix(HomeView): the language of the information of the films is changed to Spanish

--- BAZProject/MovieAPI/MovieAPI.swift
+++ BAZProject/MovieAPI/MovieAPI.swift
@@ -9,10 +9,12 @@ import Foundation
 class MovieAPI {
 
     private let apiKey: String = "f6cd5c1a9e6c6b965fdcab0fa6ddd38a"
+    private let language: String = "language=es"
     var requestType: MovieAPICategory = .Top_Rated
+    
 
     func getMovies() -> [Movie] {
-        guard let urlTrendingMovies = URL(string: "https://api.themoviedb.org/3/\(requestType.rawValue)?api_key=\(apiKey)"),
+        guard let urlTrendingMovies = URL(string: "https://api.themoviedb.org/3/\(requestType.rawValue)?api_key=\(apiKey)&\(language)"),
               let data = try? Data(contentsOf: urlTrendingMovies),
               let json = try? JSONDecoder().decode(MovieAPIResult.self, from: data)
         else {
