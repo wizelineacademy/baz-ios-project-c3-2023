da96fc4d121abdbc430269fbce672d8a907d387c
Esmeralda Angeles <Esmeralda.angeles@bancoazteca.com>
2023-02-01 23:26:02 -0600

    feat(MovieAPI): movies are consulted by region

--- BAZProject/MovieAPI/MovieAPI.swift
+++ BAZProject/MovieAPI/MovieAPI.swift
@@ -11,11 +11,12 @@ class MovieAPI {
 
     private let apiKey: String = "f6cd5c1a9e6c6b965fdcab0fa6ddd38a"
     private let language: String = "language=es"
+    private let region = "region=MX"
     var requestType: MovieAPICategory = .Top_Rated
     
 
     func getMovies() -> [Movie] {
-        guard let urlTrendingMovies = URL(string: "https://api.themoviedb.org/3/\(requestType.rawValue)?api_key=\(apiKey)&\(language)"),
+        guard let urlTrendingMovies = URL(string: "https://api.themoviedb.org/3/\(requestType.rawValue)?api_key=\(apiKey)&\(language)&\(region)"),
               let data = try? Data(contentsOf: urlTrendingMovies),
               let json = try? JSONDecoder().decode(MovieAPIResult.self, from: data)
         else {
