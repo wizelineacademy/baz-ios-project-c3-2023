2d6215f85621b991e2a1ad2e575ee7b6be7b5c69
Adrianooow96 <adrian.gorocica@elektra.com.mx>
2023-02-03 15:18:48 -0600

    add VIPER files to module TopRated

---
+++ BAZProject/Modules/TopRated/TopRatedLocalDataManager.swift
@@ -0,0 +1,14 @@
+//
+//  TopRatedLocalDataManager.swift
+//  BAZProject
+//
+//  Created by 1029187 on 02/02/23.
+//  
+//
+
+import Foundation
+
+class TopRatedLocalDataManager:TopRatedLocalDataManagerInputProtocol {
+    
+    
+}
---
+++ BAZProject/Modules/TopRated/TopRatedRemoteDataManager.swift
@@ -0,0 +1,15 @@
+//
+//  TopRatedRemoteDataManager.swift
+//  BAZProject
+//
+//  Created by 1029187 on 02/02/23.
+//  
+//
+
+import Foundation
+
+class TopRatedRemoteDataManager:TopRatedRemoteDataManagerInputProtocol {
+    
+    var remoteRequestHandler: TopRatedRemoteDataManagerOutputProtocol?
+    
+}
