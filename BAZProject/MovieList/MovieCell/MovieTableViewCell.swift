b1bad9042f9d31dc514880eb128e72246a7aff38
luis-v04 <53086124+luis-v04@users.noreply.github.com>
2023-02-03 16:40:36 -0600

    Se actualiza llave faltante

--- BAZProject/MovieList/MovieCell/MovieTableViewCell.swift
+++ BAZProject/MovieList/MovieCell/MovieTableViewCell.swift
@@ -9,7 +9,7 @@ import UIKit
 
 final class MovieTableViewCell: UITableViewCell {
     
-    static let nib: UINib = UINib(nibName: String(describing: self), bundle: .main)
+    static let nib: UINib = UINib(nibName: String(describing: MovieTableViewCell.self), bundle: .main)
     static let identifier: String = "MovieCell"
     
     @IBOutlet weak var title: UILabel!
@@ -19,15 +19,16 @@ final class MovieTableViewCell: UITableViewCell {
 
     func setCell(with movie: Movie) {
         title.text = movie.title
-        releaseDate.text = "Lazamiento: \(movie.releaseDate)"
+        releaseDate.text = "Lazamiento: \(movie.releaseDate ?? "")"
         language.text = "Idioma original: \(movie.originalLanguage)"
         guard let url = movie.getPosterURL(with: 300) else { return }
         
         DispatchQueue.global(qos: .utility).async {
             let task = URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
-            DispatchQueue.main.async {
-                if let data = data {
-                    self?.posterImage.image = UIImage(data: data)
+                DispatchQueue.main.async {
+                    if let data = data {
+                        self?.posterImage.image = UIImage(data: data)
+                    }
                 }
             }
             task.resume()
