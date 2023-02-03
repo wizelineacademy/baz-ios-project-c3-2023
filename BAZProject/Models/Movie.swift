dd09595f5b9ff64b8a90269717f1f82cf69a86ae
Andres Villanueva <andres.villanueva@elektra.com.mx>
2023-02-03 17:15:55 -0600

    replace Movie Model File

---
+++ BAZProject/Models/Movie.swift
@@ -0,0 +1,91 @@
+//
+//  Movie.swift
+//  BAZProject
+//
+//  Created by 1034209 on 02/02/23.
+//
+
+import Foundation
+
+struct MovieFetchResponse: Codable {
+    let page: Int?
+    let results: [Movie]
+    let totalPages, totalResults: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case page, results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+struct Movie: Codable {
+    let adult: Bool?
+    let backdropPath: String?
+    let id: Int?
+    let title, originalLanguage, originalTitle, overview: String?
+    let posterPath, mediaType: String?
+    let genreIDS: [Int]?
+    let popularity: Double?
+    let releaseDate: String?
+    let video: Bool?
+    let voteAverage: Double?
+    let voteCount: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case id, title
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview
+        case posterPath = "poster_path"
+        case mediaType = "media_type"
+        case genreIDS = "genre_ids"
+        case popularity
+        case releaseDate = "release_date"
+        case video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
+
+struct ReviewResponse: Codable {
+    let id, page: Int?
+    let results: [Review]
+    let totalPages, totalResults: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case id, page, results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+
+struct Review: Codable {
+    let author: String?
+    let authorDetails: AuthorDetails?
+    let content, createdAt, id, updatedAt: String?
+    let url: String?
+
+    enum CodingKeys: String, CodingKey {
+        case author
+        case authorDetails = "author_details"
+        case content
+        case createdAt = "created_at"
+        case id
+        case updatedAt = "updated_at"
+        case url
+    }
+}
+
+struct AuthorDetails: Codable {
+    let name, username, avatarPath: String?
+    let rating: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case name, username
+        case avatarPath = "avatar_path"
+        case rating
+    }
+}
