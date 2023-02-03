7390d45dfdea3cc089a77993c01ba0afe58bccc6
luis-v04 <53086124+luis-v04@users.noreply.github.com>
2023-02-03 16:15:04 -0600

    Se agrega metodo para configurar la celda

--- BAZProject/MovieList/MovieCell/MovieTableViewCell.swift
+++ BAZProject/MovieList/MovieCell/MovieTableViewCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MovieTableViewCell: UITableViewCell {
+final class MovieTableViewCell: UITableViewCell {
     
     static let nib: UINib = UINib(nibName: "MovieTableViewCell", bundle: .main)
     static let identifier: String = "MovieCell"
@@ -17,5 +17,20 @@ class MovieTableViewCell: UITableViewCell {
     @IBOutlet weak var language: UILabel!
     @IBOutlet weak var posterImage: UIImageView!
 
-    
+    func setCell(with movie: Movie) {
+        title.text = movie.title
+        releaseDate.text = "Lazamiento: \(movie.releaseDate)"
+        language.text = "Idioma original: \(movie.originalLanguage)"
+        guard let url = movie.getPosterURL(with: 300) else { return }
+        
+        DispatchQueue.global(qos: .utility).async {
+            let task = URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
+            DispatchQueue.main.async {
+                if let data = data {
+                    self?.posterImage.image = UIImage(data: data)
+                }
+            }
+            task.resume()
+        }
+    }
 }
