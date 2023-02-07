e3ace7e0a59f3c2047d2c7860eaa8d917bd865b5
Roberto Arzola <roberto.arzola@elektra.com.mx>
2023-02-07 13:40:55 -0600

    Correcciones Ent 1

--- BAZProject/Movie.swift
+++ BAZProject/Movie.swift
@@ -11,21 +11,39 @@ struct MovieResult: Codable {
 }
 
 struct Movie: Codable {
-    let adult: Bool
-    let backdrop_path: String
-    let id: Int
-    let title: String
-    let original_language: String
-    let original_title: String
-    let overview: String
-    let poster_path: String
-    let media_type: String
-    let genre_ids: [Int]
-    let popularity: Double
-    let release_date: String
-    let video: Bool
-    let vote_average: Double
-    let vote_count: Int
+    let adult: Bool?
+    let backdrop_path: String?
+    let id: Int?
+    let title: String?
+    let original_language: String?
+    let original_title: String?
+    let overview: String?
+    let poster_path: String?
+    let media_type: String?
+    let genre_ids: [Int]?
+    let popularity: Double?
+    let release_date: String?
+    let video: Bool?
+    let vote_average: Double?
+    let vote_count: Int?
+    
+    init(adult: Bool? = true, backdrop_path: String? = "", id: Int? = 0, title: String? = "", original_language: String? = "", original_title: String? = "", overview: String? = "", poster_path: String? = "", media_type: String? = "", genre_ids: [Int]? = [], popularity: Double = 0, release_date: String? = "", video: Bool? = true, vote_average: Double? = 0, vote_count: Int? = 0) {
+        self.adult = adult
+        self.backdrop_path = backdrop_path
+        self.id = id
+        self.title = title
+        self.original_language = original_language
+        self.original_title = original_title
+        self.overview = overview
+        self.poster_path = poster_path
+        self.media_type = media_type
+        self.genre_ids = genre_ids
+        self.popularity = popularity
+        self.release_date = release_date
+        self.video = video
+        self.vote_average = vote_average
+        self.vote_count = vote_count
+    }
 }
 
 enum MovieCategory: String {
@@ -48,22 +66,5 @@ enum MovieCategory: String {
         case .upcoming:
             return "/movie/upcoming"
         }
-    }
-}
-
-class BazRequest: NSObject{
-    
-    static let baseURL = "http://"
-    
-    static func getURL(_ endpoint: MovieCategory) -> String?{
-        let method = endpoint.rawValue
-        
-        if method.isEmpty{
-            return nil
-        }
-        var requestUrl: String
-        requestUrl = baseURL+method
-        
-        return requestUrl
     }
 }
--- BAZProject/MovieAPI.swift
+++ BAZProject/MovieAPI.swift
@@ -10,8 +10,6 @@ class MovieAPI {
 
     private let urlBase = "https://api.themoviedb.org/3"
     private let apiKey: String = "f6cd5c1a9e6c6b965fdcab0fa6ddd38a"
-    
-    private let urlService = BazRequest.getURL(.trending)
 
     func getMovies() -> [Movie] {
         guard let url = URL(string: urlBase+"/trending/movie/day?api_key=\(apiKey)"),
@@ -32,7 +30,7 @@ class MovieAPI {
             if let id = result.object(forKey: "id") as? Int,
                let title = result.object(forKey: "title") as? String,
                let poster_path = result.object(forKey: "poster_path") as? String {
-                movies.append(Movie(adult: true, backdrop_path: "", id: id, title: title, original_language: "", original_title: "", overview: "", poster_path: poster_path, media_type: "", genre_ids: [], popularity: 0.0, release_date: "", video: true, vote_average: 0.0, vote_count: 0))
+                movies.append(Movie(id: id, title: title, poster_path: poster_path))
             }
         }
 
