e9b79a0fe42e8846d8f48cb056235b9a6139cc1c
GERARDO BAUTISTA CASTANEDA <gerardo.bautistac@elektra.com.mx>
2023-02-14 13:31:42 -0600

    remove uri logo

--- BAZProject/Util/Localized/LocalizedKey.swift
+++ BAZProject/Util/Localized/LocalizedKey.swift
@@ -8,8 +8,6 @@
 import Foundation
 
 extension String {
-    static let urlLogo = "https://www.roc21.com/blog/wp-content/uploads/2019/07/logo-cinepolis-dos.jpg"
-    
     // MARK: - TheMovieDb API
     static let apiKeyTheMovieDb = "f6cd5c1a9e6c6b965fdcab0fa6ddd38a"
     static let theMovieDbBasePath = "https://api.themoviedb.org/3"
