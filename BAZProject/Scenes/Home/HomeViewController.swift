92062f179253ae8cdb6c096b3bad087e20796a2f
Andres Villanueva <andres.villanueva@elektra.com.mx>
2023-02-14 17:51:21 -0600

    add scrollView and stackView in HomeViewController

--- BAZProject/Scenes/Home/HomeViewController.swift
+++ BAZProject/Scenes/Home/HomeViewController.swift
@@ -18,8 +18,23 @@ class HomeViewController: UIViewController {
     var interactor: HomeBusinessLogic?
     var router: (HomeRoutingLogic & HomeDataPassing)?
     
-    // MARK: IBOutlet
-    @IBOutlet weak var section1: MoviesSectionView!
+    // MARK: Properties
+    var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    var scrollViewContainer: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.axis = .vertical
+        stackView.spacing = 5
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    private let sections: [fetchMoviesTypes] = [.nowPlaying, .popular, .topRated, .trending, .upComing]
+    
     
     // MARK: Init
     required init?(coder: NSCoder) {
@@ -29,15 +44,44 @@ class HomeViewController: UIViewController {
     
     // MARK: Life Cycle
     override func viewDidLoad() {
-
+        super.viewDidLoad()
+        addScrollViewToView()
+        loadMoviesSectionViews()
     }
     override func viewWillAppear(_ animated: Bool) {
-
+        super.viewWillAppear(animated)
     }
     
     // MARK: Setup
     func setup() {
         // TODO: setup VIP Module
+    }
+    
+    private func loadMoviesSectionViews() {
+        sections.forEach { section in
+            addMoviesSectionView(section: section)
+        }
+    }
+    
+    private func addMoviesSectionView(section: fetchMoviesTypes) {
+        let sectionView = MoviesSectionView()
+        sectionView.heightAnchor.constraint(equalToConstant: view.frame.height / 3).isActive = true
+        scrollViewContainer.addArrangedSubview(sectionView)
+    }
+    
+    private func addScrollViewToView() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(scrollViewContainer)
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+
+        scrollViewContainer.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        scrollViewContainer.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+        scrollViewContainer.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        scrollViewContainer.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        scrollViewContainer.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
     }
 }
 
