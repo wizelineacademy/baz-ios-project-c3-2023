8a8636b82b371bce61524dc82546f22b196ea5da
GERARDO BAUTISTA CASTANEDA <gerardo.bautistac@elektra.com.mx>
2023-02-14 21:01:57 -0600

    add movie detail response

---
+++ BAZProject/Modules/Detail/Entities/MovieDetail.swift
@@ -0,0 +1,102 @@
+//
+//  MovieDetail.swift
+//  BAZProject
+//
+//  Created by 1058889 on 14/02/23.
+//
+import Foundation
+
+// MARK: - MovieDetailResult
+struct MovieDetailResult: Codable {
+    var adult: Bool?
+    var backdropPath: String?
+    var belongsToCollection: BelongsToCollection?
+    var budget: Int?
+    var genres: [Genre]?
+    var homepage: String?
+    var id: Int?
+    var imdbID, originalLanguage, originalTitle, overview: String?
+    var popularity: Double?
+    var posterPath: String?
+    var productionCompanies: [ProductionCompany]?
+    var productionCountries: [ProductionCountry]?
+    var releaseDate: String?
+    var revenue, runtime: Int?
+    var spokenLanguages: [SpokenLanguage]?
+    var status, tagline, title: String?
+    var video: Bool?
+    var voteAverage: Double?
+    var voteCount: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case belongsToCollection = "belongs_to_collection"
+        case budget, genres, homepage, id
+        case imdbID = "imdb_id"
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview, popularity
+        case posterPath = "poster_path"
+        case productionCompanies = "production_companies"
+        case productionCountries = "production_countries"
+        case releaseDate = "release_date"
+        case revenue, runtime
+        case spokenLanguages = "spoken_languages"
+        case status, tagline, title, video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
+
+// MARK: - BelongsToCollection
+struct BelongsToCollection: Codable {
+    var id: Int?
+    var name, posterPath, backdropPath: String?
+
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case posterPath = "poster_path"
+        case backdropPath = "backdrop_path"
+    }
+}
+
+// MARK: - Genre
+struct Genre: Codable {
+    var id: Int?
+    var name: String?
+}
+
+// MARK: - ProductionCompany
+struct ProductionCompany: Codable {
+    var id: Int?
+    var logoPath, name, originCountry: String?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case logoPath = "logo_path"
+        case name
+        case originCountry = "origin_country"
+    }
+}
+
+// MARK: - ProductionCountry
+struct ProductionCountry: Codable {
+    var iso3166_1, name: String?
+
+    enum CodingKeys: String, CodingKey {
+        case iso3166_1 = "iso_3166_1"
+        case name
+    }
+}
+
+// MARK: - SpokenLanguage
+struct SpokenLanguage: Codable {
+    var englishName, iso639_1, name: String?
+
+    enum CodingKeys: String, CodingKey {
+        case englishName = "english_name"
+        case iso639_1 = "iso_639_1"
+        case name
+    }
+}
