aa6dc88750c8bca5118853b459a6b3bdec6adbbd
Adrianooow96 <goca_dts@outlook.com>
2023-02-16 11:17:38 -0600

    create searching module files

---
+++ BAZProject/Modules/Searching/SearchingPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  SearchingPresenter.swift
+//  BAZProject
+//
+//  Created by 1029187 on 16/02/23.
+//
+
+import Foundation
---
+++ BAZProject/Modules/Searching/SearchingView.swift
@@ -0,0 +1,8 @@
+//
+//  SearchingView.swift
+//  BAZProject
+//
+//  Created by 1029187 on 16/02/23.
+//
+
+import Foundation
