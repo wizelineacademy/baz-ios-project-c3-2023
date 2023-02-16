b13ab6b46387eb50cd513023528fb4064cb4cb1e
Adrianooow96 <goca_dts@outlook.com>
2023-02-16 11:20:33 -0600

    Create protocols for searching module

--- BAZProject/Modules/Searching/SearchingProtocols.swift
+++ BAZProject/Modules/Searching/SearchingProtocols.swift
@@ -5,4 +5,44 @@
 //  Created by 1029187 on 16/02/23.
 //
 
-import Foundation
+import UIKit
+
+protocol SearchingViewProtocol: AnyObject {
+    // PRESENTER -> VIEW
+    var presenter: SearchingPresenterProtocol? { get set }
+    
+    func reloadData()
+}
+
+protocol SearchingRouterProtocol: AnyObject {
+    // PRESENTER -> ROUTER
+    static func createSearchingModule() -> UIViewController
+}
+
+protocol SearchingPresenterProtocol: AnyObject {
+    // VIEW -> PRESENTER
+    var view: SearchingViewProtocol? { get set }
+    var interactor: SearchingInteractorInputProtocol? { get set }
+    var router: SearchingRouterProtocol? { get set }
+    
+    func notifyViewLoaded()
+}
+
+protocol SearchingInteractorOutputProtocol: AnyObject {
+    // INTERACTOR -> PRESENTER
+}
+
+protocol SearchingInteractorInputProtocol: AnyObject {
+    // PRESENTER -> INTERACTOR
+    var presenter: SearchingInteractorOutputProtocol? { get set }
+    var remoteDatamanager: SearchingRemoteDataManagerInputProtocol? { get set }
+}
+
+protocol SearchingRemoteDataManagerInputProtocol: AnyObject {
+    // INTERACTOR -> REMOTEDATAMANAGER
+    var remoteRequestHandler: TopRatedRemoteDataManagerOutputProtocol? { get set }
+}
+
+protocol SearchingRemoteDataManagerOutputProtocol: AnyObject {
+    // REMOTEDATAMANAGER -> INTERACTOR
+}
