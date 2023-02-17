a2c4ba2dc69193625667e349ba94e141384c2a01
Andres Villanueva <andres.villanueva@elektra.com.mx>
2023-02-17 15:43:08 -0600

    create Search Storyboard View and add UISearchBar

--- BAZProject/Scenes/SearchMovies/SearchMoviesViewController.swift
+++ BAZProject/Scenes/SearchMovies/SearchMoviesViewController.swift
@@ -10,8 +10,41 @@ import UIKit
 
 protocol SearchMoviesDisplayLogic: AnyObject {
     // TODO: create functions to manage display logic
+    
 }
 
-class SearchMoviesViewController: UIViewController, SearchMoviesDisplayLogic {
+class SearchMoviesViewController: UIViewController {
+    
+    // MARK: Properties
+    lazy var searchBar: UISearchBar? = {
+        let view = UISearchBar(frame: .zero)
+        view.sizeToFit()
+        view.placeholder = "Search"
+        view.becomeFirstResponder()
+        
+        return view
+    }()
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSearchViewInNavigation()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    private func addSearchViewInNavigation() {
+        guard let searchBar = searchBar else {
+            return
+        }
+        let rightButton = UIBarButtonItem(customView: searchBar)
+        self.navigationItem.rightBarButtonItem = rightButton
+    }
+}
+
+extension SearchMoviesViewController: SearchMoviesDisplayLogic {
     // TODO: conform SearchMoviesDisplayLogic protocol
+
 }
