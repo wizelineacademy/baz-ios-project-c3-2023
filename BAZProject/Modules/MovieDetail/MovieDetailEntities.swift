e1374a7ad621418a66102e8f77dfa44f8536a2df
Adrianooow96 <goca_dts@outlook.com>
2023-02-21 23:49:25 -0600

    add struct for movie details

--- BAZProject/Modules/MovieDetail/MovieDetailEntities.swift
+++ BAZProject/Modules/MovieDetail/MovieDetailEntities.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct MovieDetail {
+    let id: Int?
+    let title: String?
+    let overview: String?
+    let popularity: Double?
+    let posterPath: String?
+    let releaseDate: String?
+    let status: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case overview
+        case popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case status
+    }
+}
