11dfe73bbd8f5370de8869b00cc69b811286ec77
Adrianooow96 <goca_dts@outlook.com>
2023-02-21 23:20:56 -0600

    create protocols for MovieDetail module

--- BAZProject/Modules/MovieDetail/MovieDetailProtocols.swift
+++ BAZProject/Modules/MovieDetail/MovieDetailProtocols.swift
@@ -6,3 +6,47 @@
 //
 
 import UIKit
+
+protocol MovieDetailViewProtocol: AnyObject {
+    // PRESENTER -> VIEW
+    var presenter: MovieDetailPresenterProtocol? { get set }
+    
+}
+
+protocol MovieDetailRouterProtocol: AnyObject {
+    // PRESENTER -> ROUTER
+    static func createMovieDetailModule() -> UIViewController
+}
+
+protocol MovieDetailPresenterProtocol: AnyObject {
+    // VIEW -> PRESENTER
+    var view: MovieDetailViewProtocol? { get set }
+    var interactor: MovieDetailInteractorInputProtocol? { get set }
+    var router: MovieDetailRouterProtocol? { get set }
+    var searchResults: [SearchResult]? { get set }
+    
+    func notifyViewLoaded()
+}
+
+protocol MovieDetailInteractorOutputProtocol: AnyObject {
+    // INTERACTOR -> PRESENTER
+    
+}
+
+protocol MovieDetailInteractorInputProtocol: AnyObject {
+    // PRESENTER -> INTERACTOR
+    var presenter: MovieDetailInteractorOutputProtocol? { get set }
+    var remoteDatamanager: MovieDetailRemoteDataManagerInputProtocol? { get set }
+    
+}
+
+protocol MovieDetailRemoteDataManagerInputProtocol: AnyObject {
+    // INTERACTOR -> REMOTEDATAMANAGER
+    var remoteRequestHandler: MovieDetailRemoteDataManagerOutputProtocol? { get set }
+    
+}
+
+protocol MovieDetailRemoteDataManagerOutputProtocol: AnyObject {
+    // REMOTEDATAMANAGER -> INTERACTOR
+    
+}
