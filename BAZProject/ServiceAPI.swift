2f42734845de2fde27fc074f230b63bed1150b4d
Adrianooow96 <goca_dts@outlook.com>
2023-02-22 00:16:51 -0600

    modify endpoints to return content in spanish

--- BAZProject/ServiceAPI.swift
+++ BAZProject/ServiceAPI.swift
@@ -86,15 +86,16 @@ enum ServiceError: Error {
 class MovieRequest: NSObject {
     static let baseURL: String = "https://api.themoviedb.org/3/"
     static let apiKey: String = "f6cd5c1a9e6c6b965fdcab0fa6ddd38a"
+    static let lenguage: String = "es-MX"
     
     static func getURL(endpoint: Endpoint) -> URL? {
         let endpoint = endpoint.rawValue
-        let requestURL: String = baseURL+endpoint+"?api_key=\(apiKey)"
+        let requestURL: String = baseURL+endpoint+"?api_key=\(apiKey)&language=\(lenguage)"
         return URL(string: requestURL.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")
     }
     
     static func searchMovie(search query: String, allowAdultResults: Bool = false) -> URL? {
-        let endpoint = Endpoint.searchMovies.rawValue+"?api_key=\(apiKey)"
+        let endpoint = Endpoint.searchMovies.rawValue+"?api_key=\(apiKey)&language=\(lenguage)"
         let requestURL: String = baseURL+endpoint+"&query=\(query)&include_adult=\(allowAdultResults)"
         return URL(string: requestURL.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")
     }
