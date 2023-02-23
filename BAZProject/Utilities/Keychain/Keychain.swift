cc1c29d0b07fd994f1fdd0bfbc009dfb3df344d9
Leobardo Gama <leobardo.gama@elektra.com.mx>
2023-02-23 13:04:28 -0600

    Keychain[Create]: Add keychain

---
+++ BAZProject/Utilities/Keychain/Keychain.swift
@@ -0,0 +1,91 @@
+//
+//  Keychain.swift
+//  BAZProject
+//
+//  Created by Leobardo Gama Muñoz on 23/02/23.
+//
+
+import Foundation
+
+final class KeychainHelper {
+    
+    static let standard = KeychainHelper()
+    private init() {}
+    
+    // Class implementation here...
+    
+    private func save(_ data: Data, service: String, account: String) {
+
+        // Create query
+        let query = [
+            kSecValueData: data,
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service,
+            kSecAttrAccount: account,
+        ] as CFDictionary
+
+        // Add data in query to keychain
+        let status = SecItemAdd(query, nil)
+
+        if status != errSecSuccess {
+            // Print out the error
+            debugPrint("Error: \(status)")
+        }
+    }
+
+    private func read(service: String, account: String) -> Data? {
+
+        let query = [
+            kSecAttrService: service,
+            kSecAttrAccount: account,
+            kSecClass: kSecClassGenericPassword,
+            kSecReturnData: true
+        ] as CFDictionary
+
+        var result: AnyObject?
+        SecItemCopyMatching(query, &result)
+
+        return (result as? Data)
+    }
+    
+    func delete(service: String, account: String) {
+        
+        let query = [
+            kSecAttrService: service,
+            kSecAttrAccount: account,
+            kSecClass: kSecClassGenericPassword,
+            ] as CFDictionary
+        
+        // Delete item from keychain
+        SecItemDelete(query)
+    }
+    
+    func save<T>(_ item: T, service: String, account: String) where T : Codable {
+        
+        do {
+            // Encode as JSON data and save in keychain
+            let data = try JSONEncoder().encode(item)
+            save(data, service: service, account: account)
+            
+        } catch {
+            debugPrint("Fail to encode item for keychain: \(error)")
+        }
+    }
+    
+    func read<T>(service: String, account: String, type: T.Type) -> T? where T : Codable {
+        
+        // Read item data from keychain
+        guard let data = read(service: service, account: account) else {
+            return nil
+        }
+        
+        // Decode JSON data to object
+        do {
+            let item = try JSONDecoder().decode(type, from: data)
+            return item
+        } catch {
+            debugPrint("Fail to decode item for keychain: \(error)")
+            return nil
+        }
+    }
+}
