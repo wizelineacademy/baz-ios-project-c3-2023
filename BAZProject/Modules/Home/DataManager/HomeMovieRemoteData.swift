f10225bdcd0d583eb8f877ddf7d8b0ea162f90f5
ipaws <uglypawis@hotmail.com>
2023-03-01 00:45:57 -0600

    main view Refactor

---
+++ BAZProject/Modules/Home/DataManager/HomeMovieRemoteData.swift
@@ -0,0 +1,8 @@
+//
+//  HomeMovieRemoteData.swift
+//  BAZProject
+//
+//  Created by Brenda Paola Lara Moreno on 24/02/23.
+//
+
+import Foundation
---
+++ BAZProject/Modules/Home/View/MovieCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCollectionViewCell.swift
+//  BAZProject
+//
+//  Created by Brenda Paola Lara Moreno on 28/02/23.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
