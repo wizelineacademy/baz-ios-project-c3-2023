b4e4af379ad123c9e3e100d2e7079bcb40954641
GERARDO BAUTISTA CASTANEDA <gerardo.bautistac@elektra.com.mx>
2023-03-02 17:47:22 -0600

    add documentation on CellMovieTop

--- BAZProject/Components/ImagesSlider/Cells/CellMovieTop/CellMovieTop.swift
+++ BAZProject/Components/ImagesSlider/Cells/CellMovieTop/CellMovieTop.swift
@@ -7,12 +7,20 @@
 
 import UIKit
 
+/// This class allows you to create a cell with a image and a title.
+///
+/// :conditions: The methods it has are those declared in the class 'CellMovieTop':
+/// * func setData
+///
 final class CellMovieTop: UICollectionViewCell {
     static let  identifier: String = .cellMovieTopXibIdentifier
+    /// This function allows to get the UINib of the cell.
+    /// Way to call CellMovieTop.nib()
     static func nib() -> UINib {
         return UINib(nibName: identifier, bundle: nil)
     }
 
+    // MARK: - Declaration IBOutlets
     @IBOutlet weak private var photoImageView: UIImageView!
     @IBOutlet weak private var titleLabel: UILabel!
 
@@ -20,6 +28,9 @@ final class CellMovieTop: UICollectionViewCell {
         super.awakeFromNib()
     }
 
+    /// This function allows you to configure the cell, where you must indicate the cellMovieType.
+    ///
+    /// :param: cellMovieType CellMovieType indicating the CellMovieType
     func setData(cellMovieType: CellMovieType) {
         photoImageView.loadImage(id: cellMovieType.imageUrlString)
         titleLabel.text = cellMovieType.title
